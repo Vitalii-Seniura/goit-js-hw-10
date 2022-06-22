b423a76ec74043dc68eb8b5786922a48ae722242
Vitalii-Seniura <senyura.w@gmail.com>
2022-06-23 01:04:41 +0300

    7

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
-export const fetchCountries = name => {
-  return fetch(`https://restcountries.com/v2/all?fields=${name}.official,capital,population,flags.svg,languages
+export const fetchCountries = (name) => {
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages
 `).then(response => {
     if (!response.ok) {
       throw new Error(response.statusText);
@@ -7,3 +7,5 @@ export const fetchCountries = name => {
     return response.json();
   });
 };
+
+
--- src/index.js
+++ src/index.js
@@ -1,40 +1,68 @@
 import './css/styles.css';
-import { fetchCountries } from  "./fetchCountries"; 
-
-var debounce = require('lodash.debounce');
-
-import Notiflix from 'notiflix';
+import {fetchCountries} from  "./fetchCountries"; 
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
-
-
-const form = document.querySelector('#search-box');
+const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-form.addEventListener('input', event => {
-  event.preventDefault();
 
-  fetchCountries(form.elements.name.value)
-    .then(country => {
-      profileContainer.insertAdjacentHTML = buildProfileMarkup(country);
+
+
+input.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY)
+);
+
+function onInput() {
+  const name = input.value.trim();
+  console.log(name);
+  if (name === "") {
+    return (countryList.innerHTML = ''),
+      (countryInfo.innerHTML = '')
+  }
+
+
+    fetchCountries(name)
+    .then(countries => {
+      countryList.innerHTML = ''
+      countryInfo.innerHTML = ''
+      if (countries.length === 1) {
+        countryList.insertAdjacentHTML('beforeend', createMarkupList(countries))
+        countryInfo.insertAdjacentHTML('beforeend', createMarkupInfo(countries))
+      } else if (countries.length >= 10) {
+        alertManyFound()
+      } else {
+       countryList.insertAdjacentHTML('beforeend', createMarkupList(countries))
+      }
     })
-    .catch(() => {
-      profileContainer.insertAdjacentHTML = buildErrorScreen();
-    });
-
-  form.reset();
-});
+    .catch(alertNotFound);
+}
 
 
 
-
-
-
-
-
-
-export const buildErrorScreen = () => {
-  return `<b class="error-screen">Такого пользователя не существует :(</b>`;
-};
-
+function createMarkupInfo(countries){
+    const markup = countries.map(({name, capital, population, languages, flags}) => {
+       return `
+        <ul class="country-info__list">
+       <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+       <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+       <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
+   </ul>`;}).join("");
+       return markup;
+ };
+ function createMarkupList(countries){
+    const markupInfo = countries.map(({name, flags}) => {
+               return `
+               <li class="country-list__item">
+               <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+               <h2 class="country-list__name">${name.official}</h2>
+           </li>`;}).join("");
+               return markupInfo;
+     }
+    function alertManyFound(){
+         return Notify.info('Too many matches found. Please enter a more specific name.');
+    }
+    function alertNotFound(){
+        Notify.failure('"Oops, there is no country with that name');
+    }
