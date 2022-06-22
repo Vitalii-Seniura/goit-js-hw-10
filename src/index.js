c43d43de6dad36ef92d4f3e2f51bb422557e2284
Vitalii-Seniura <senyura.w@gmail.com>
2022-06-22 22:01:38 +0300

    6

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
 
-const form = document.querySelector('.search-box');
+const form = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
